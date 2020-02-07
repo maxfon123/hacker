b07fc6bf3e892fe839d3d3af7c90b94b12e1fc02
maxhacker00 <60785508+maxhacker00@users.noreply.github.com>
2020-02-07 22:16:55 +0200

    Create learn.py

---
+++ learn.py
@@ -0,0 +1,9 @@
+
+
+def test_sum():
+    name = input("Enter Your Name :")
+    age = input("Enter Age : ")
+    print("Your Name Is" , name , "Age Is " , age)
+    print("Hillo " , name)
+    
+test_sum()
